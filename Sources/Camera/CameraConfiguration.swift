1103f8ca3c6a074333935c20bd96d38d8892386b
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-04 00:58:29 +0500

    tag increment

--- Sources/Camera/CameraConfiguration.swift
+++ Sources/Camera/CameraConfiguration.swift
@@ -1,7 +1,7 @@
 import AVKit
 
 public struct CameraConfiguration: Equatable {
-    var deviceType: AVCaptureDevice.DeviceType = .builtInDualCamera
+    var deviceType: AVCaptureDevice.DeviceType = .builtInTripleCamera
     var position: AVCaptureDevice.Position = .back
     var flashMode: AVCaptureDevice.FlashMode = .off
     var torchMode: AVCaptureDevice.TorchMode = .off
