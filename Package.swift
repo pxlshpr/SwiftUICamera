3809bb7d2f16e3f14d4e7fb1a72e004d2d7270d1
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-22 13:32:05 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/pxlshpr/SwiftHaptics", from: "0.1.0"),
+        .package(url: "https://github.com/pxlshpr/SwiftHaptics", from: "0.1.2"),
         .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.172"),
         .package(url: "https://github.com/pxlshpr/VisionSugar", from: "0.0.71"),
         .package(url: "https://github.com/pxlshpr/FoodLabelScanner", from: "0.0.67"),
