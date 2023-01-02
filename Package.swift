e3404871c4a76bbeb7dd3e0a6fbbfc49f5142cbd
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-03 01:13:45 +0500

    bump

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/pxlshpr/SwiftHaptics", from: "0.1.2"),
-        .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.172"),
-        .package(url: "https://github.com/pxlshpr/VisionSugar", from: "0.0.71"),
+        .package(url: "https://github.com/pxlshpr/SwiftHaptics", from: "0.1.3"),
+        .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.320"),
+        .package(url: "https://github.com/pxlshpr/VisionSugar", from: "0.0.75"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
